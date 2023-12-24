d9ad3ff4ba2dd6686631df283fc4fe04d3ca7410
quranteaching23 <husseinfauziyya@gmial.com>
2023-12-24 19:57:52 +0100

    modules import

--- src/app/Pages/onboard-gs/onboard-gs.component.ts
+++ src/app/Pages/onboard-gs/onboard-gs.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field'
 
+
+ 
 @Component({
   selector: 'app-onboard-gs',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    CommonModule
+  ],
   templateUrl: './onboard-gs.component.html',
   styleUrl: './onboard-gs.component.css'
 })
 export class OnboardGsComponent {
+  gsForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(){
+    this.gsForm = this.fb.group({
+      fn: ['', [ Validators.minLength(3)]],
+      ln: ['', [ Validators.min(3),  /*Validators.required */]]
+    })
+  }
 }
