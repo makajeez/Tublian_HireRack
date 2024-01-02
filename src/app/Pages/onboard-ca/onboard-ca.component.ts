0183858d5d9333179f0a5ebb21d5bbf9aa9561d7
Makajeez <buhari.alhassan0@gmail.com>
2024-01-02 01:21:18 +0100

    Read data from router history state

--- src/app/Pages/onboard-ca/onboard-ca.component.ts
+++ src/app/Pages/onboard-ca/onboard-ca.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule }  from '@angular/router'
+import { RouterModule, ActivatedRoute, Router }  from '@angular/router'
 
 @Component({
   selector: 'app-onboard-ca',
@@ -16,14 +16,19 @@ import { RouterModule }  from '@angular/router'
   styleUrl: './onboard-ca.component.css'
 })
 export class OnboardCaComponent {
-  gsForm!: FormGroup;
+  caForm!: FormGroup;
+  fn = '';
+  ln = '';
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private router: Router, private actRouter: ActivatedRoute) {
+  }
 
   ngOnInit(){
-    this.gsForm = this.fb.group({
-      fn: ['', [ Validators.minLength(3), Validators.required]],
-      ln: ['', [ Validators.minLength(3), Validators.required ]]
+    this.fn = window.history.state.fn
+    this.ln = window.history.state.ln
+    this.caForm = this.fb.group({
+      email: ['', [ Validators.email, Validators.required]],
+      pw: ['', [ Validators.minLength(8), Validators.required, Validators.pattern("^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$")]]
     })
   }
 }
